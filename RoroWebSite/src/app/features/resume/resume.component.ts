a5ba7cb72fe93fb31fa67552d824175a44ce9025
R0bin-sys <robinmartres@gmail.com>
2025-04-26 15:14:33 +0200

    Scroll resume OK

--- RoroWebSite/src/app/features/resume/resume.component.ts
+++ RoroWebSite/src/app/features/resume/resume.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ScrollCirclesComponent } from "./scroll-circles/scroll-circles.component";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-resume',
-  imports: [ScrollCirclesComponent],
+  imports: [ScrollCirclesComponent, MatIconModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
--- RoroWebSite/src/app/features/resume/scroll-circles/scroll-circles.component.ts
+++ RoroWebSite/src/app/features/resume/scroll-circles/scroll-circles.component.ts
@@ -7,6 +7,8 @@ import {
 } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { createNoise2D } from 'simplex-noise';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 /**
  * Cv avec effet de cercles évoluant
@@ -27,7 +29,7 @@ export class ScrollCirclesComponent implements OnInit {
   constructor(private readonly sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
-    const nbCircles: number = 1500;
+    const nbCircles: number = 1250;
     const ceiling: number = 100;
     const floor: number = 0;
     let step: number = 1;
@@ -37,17 +39,84 @@ export class ScrollCirclesComponent implements OnInit {
 
     for (let index: number = 0; index < nbCircles; index++) {
       this.addCircle(pctCircle, index);
+      const moduloStep = 250;
       //Le cercle est-il une étape (tous les 250 cercles)
-      if (index % 250 === 0 && index !== 0) {
+      if (index % moduloStep === 0 && index !== 0) {
         this.addStepCircle(pctCircle, index);
-        }
-        
-        // Gestion du pourcentage de la couleur du cercle
+      }
+
+      // Gestion du pourcentage de la couleur du cercle
       pctCircle += step;
       if (pctCircle === ceiling || pctCircle === floor) {
         step = -step;
       }
     }
+
+    gsap.registerPlugin(ScrollTrigger);
+  }
+
+  ngAfterViewInit() {
+    this.addAnimations();
+  }
+
+  private addAnimations() {
+    const Circles = document.querySelectorAll('.circle');
+    const StepCircles = document.querySelectorAll('.step-circle');
+    const Steps = document.querySelectorAll('.step');
+    const Lines = document.querySelectorAll('.stepLine');
+
+    // Cercles
+    const circleTimeLine = gsap.timeline({
+      scrollTrigger: {
+        scrub: 1,
+        start: 'top top',
+        end: 'bottom center',
+        fastScrollEnd: true,
+        preventOverlaps: true,
+        toggleActions: 'restart none none reverse',
+      },
+    });
+    Circles.forEach((circle) => {
+      circleTimeLine.from(circle, {
+        opacity: 0,
+      });
+    });
+
+    StepCircles.forEach((step_circle) => {
+      gsap.from(step_circle, {
+        scrollTrigger: {
+          trigger: step_circle,
+          start: 'top center',
+          toggleActions: 'restart none none reverse',
+        },
+        scale: 0,
+        ease: 'back',
+      });
+    });
+
+    //   Animations des étapes
+    Steps.forEach((step) => {
+      gsap.from(step, {
+        opacity: 0,
+        scrollTrigger: {
+          trigger: step,
+          start: 'top center',
+          toggleActions: 'restart none none reverse',
+        },
+      });
+    });
+
+    Lines.forEach((line) => {
+      gsap.from(line, {
+        opacity: 0,
+        x: -100,
+        scrollTrigger: {
+          trigger: line,
+          start: 'top center',
+          toggleActions: 'restart none none reverse',
+        },
+      });
+    });
   }
 
   private addCircle(pctCircle: number, index: number) {
@@ -84,9 +153,17 @@ export class ScrollCirclesComponent implements OnInit {
   private addStepCircle(pctCircle: number, index: number) {
     //   Création du cercle
     let circleDiv = document.createElement('div');
-    circleDiv.classList.add('step-circle', 'circle');
+    circleDiv.classList.add('step-circle', 'circle2');
+    circleDiv.style.transform = `translate(${
+      this.noise2D(index * 0.002, index * 0.001) * 50
+    }px)`;
+
+    //Ligne de ssépration
+    let stepLine = document.createElement('div');
+    stepLine.classList.add('stepLine');
 
     // Ajout du cercle au contenu
     this.addElementToMainContainer(circleDiv);
+    this.addElementToMainContainer(stepLine);
   }
 }
